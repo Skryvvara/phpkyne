42c6f050541482aa10bde5aba55215840aea1279
Dimitri Kaiser <darkyne.gaming@outlook.de>
2021-04-07 16:35:26 +0200

    improved style

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<footer id="main-footer">
+    <div class="container flex-row">
+        <ul class="footer-list">
+            <li>&copy; 2021 - <a href="/" class="app-link">Darkyne</a></li>
+            <li><a href="/contact" class="app-link">Contact</a></li>
+        </ul>
+    </div>
+</footer>
--- resources/views/work.blade.php
+++ resources/views/work.blade.php
@@ -6,19 +6,19 @@
     <section class="main-section">
         <div class="container">
             <h1 class="section-title">Projects</h1>
-            @include('components/warning', ['title' => 'Caution!', 'message' => 'Not all projects are listed here and will be added soon.'])
+            @include('components/warning', ['title' => 'Caution!', 'message' => 'Not all projects are listed here yet and will be added soon.'])
             <div class="project-grid">
             @foreach($projects as $project)
-            <div class="project-card">
+            <div class="card project-card">
                 <div class="project-header">
-                    <h2>{{$project->projectName}}</h2>
+                    <h1>{{$project->projectName}}</h1>
                     <span class="tag tag-{{$project->projectTag}}">{{strtoupper($project->projectTag)}}</span>
                 </div>
                 <div class="project-body">
                     <p>{{$project->projectDesc}}</p>
                 </div>
                 <div class="project-footer">
-                    <a href="{{$project->projectUrl}}" class="app-button">Source<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M13 3L16.293 6.293 9.293 13.293 10.707 14.707 17.707 7.707 21 11 21 3z"/><path d="M19,19H5V5h7l-2-2H5C3.897,3,3,3.897,3,5v14c0,1.103,0.897,2,2,2h14c1.103,0,2-0.897,2-2v-5l-2-2V19z"/></svg></a>
+                    <a href="{{$project->projectUrl}}" class="app-button-inverted">Source<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M13 3L16.293 6.293 9.293 13.293 10.707 14.707 17.707 7.707 21 11 21 3z"/><path d="M19,19H5V5h7l-2-2H5C3.897,3,3,3.897,3,5v14c0,1.103,0.897,2,2,2h14c1.103,0,2-0.897,2-2v-5l-2-2V19z"/></svg></a>
                 </div>
             </div>
             @endforeach
