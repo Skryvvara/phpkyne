1552125a8863b1389fcdb78f24ddcaad56806c80
Dimitri Kaiser <darkyne.gaming@outlook.de>
2021-05-30 23:43:56 +0200

    changed User model to use UUID instead of AI integer ID

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\UsesUUID;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,6 +10,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
+    use UsesUUID;
     use HasFactory, Notifiable;
 
     /**
